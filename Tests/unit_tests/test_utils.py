e4add8460bbd978ed374f9da081a5bfeff485f05
D0948291 <jflayo901023@gmail.com>
2022-12-13 17:34:35 +0800

    pytest1

--- Tests/unit_tests/test_utils.py
+++ Tests/unit_tests/test_utils.py
@@ -37,3 +37,11 @@ class BmiCalculatorTest(TestCase):
         bmi, bmi_means = bmi_calculator(height, weight)
         self.assertEqual(bmi, 21.48)
         self.assertEqual(bmi_means, '健康體位')
+    
+    def test_bmi_result_heavy(self):
+        """Tests bmi result."""
+        height = 1.7
+        weight = 75
+        bmi, bmi_means = bmi_calculator(height, weight)
+        self.assertEqual(bmi, 25.9)
+        self.assertEqual(bmi_means, '過重')
